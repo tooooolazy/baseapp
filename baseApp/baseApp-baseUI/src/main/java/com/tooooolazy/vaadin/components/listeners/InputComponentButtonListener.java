f44a78443c952e1fbaf055d78fb934d8847e3ecd
gpatoulas <gpatoulas.icap.gr>
2019-02-25 13:34:30 +0200

    Base Listener for InputComponent

---
+++ baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/vaadin/components/listeners/InputComponentButtonListener.java
@@ -0,0 +1,104 @@
+package com.tooooolazy.vaadin.components.listeners;
+
+import com.tooooolazy.util.Messages;
+import com.tooooolazy.vaadin.components.InputComponent;
+import com.tooooolazy.vaadin.exceptions.DataRequiredException;
+import com.vaadin.data.BinderValidationStatus;
+import com.vaadin.server.ErrorMessage;
+import com.vaadin.shared.ui.ErrorLevel;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+
+/**
+ * Base class for handling the submit button in an input component
+ * @author gpatoulas
+ *
+ */
+public class InputComponentButtonListener implements Button.ClickListener {
+	protected InputComponent ic;
+	protected BinderValidationStatus bvs;
+
+	protected void init() {
+		
+	}
+	protected void getInputComponent(ClickEvent event) {
+		Component c = event.getComponent();
+		while (!(c instanceof InputComponent)) {
+			c = c.getParent();
+		}
+		ic = (InputComponent) c;
+	}
+	public boolean isValid() {
+		bvs = ic.getBinder().validate();
+
+		return bvs.isOk();
+	}
+	protected Object submit() throws Exception {
+		return null;
+	}
+	protected void onError(Exception e) {
+		ic.setComponentError(createError(ic.getClass(), e));
+		ic.enableSubmit();
+	}
+	protected void onSuccess(Object res) {
+		
+	}
+	@Override
+	public void buttonClick(ClickEvent event) {
+		init();
+
+		getInputComponent(event);
+
+		Exception le = null;
+		Object res = null;
+
+		try {
+			if (isValid()) {
+				try {
+					ic.setComponentError(null);
+
+					res = submit();
+				} catch (Exception e) {
+					e.printStackTrace();
+					if (e instanceof RuntimeException && e.getCause() != null) {
+						e = (Exception)e.getCause();
+					}
+						
+					le = e;
+				}
+	
+				if (le != null) {
+					onError(le);
+					return;
+				} else {
+					onSuccess(res);
+				}
+			} else {
+				le = new DataRequiredException();
+				onError(le);
+			}
+		} catch (Exception e) {
+			onError(e);
+		}
+	}
+
+	public static ErrorMessage createError(final Class c, final Exception e) {
+		ErrorMessage er = new ErrorMessage() {
+			@Override
+			public String getFormattedHtmlMessage() {
+				if (e == null)
+					return Messages.getString(c, "error");
+				else {
+					return Messages.getString(e.getClass(), "msg");
+				}
+			}
+			
+			@Override
+			public ErrorLevel getErrorLevel() {
+				return ErrorLevel.ERROR;
+			}
+		};
+		return er;
+	}
+}
--- baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/vaadin/views/MainView.java
+++ baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/vaadin/views/MainView.java
@@ -1,6 +1,7 @@
 package com.tooooolazy.vaadin.views;
 
 import com.tooooolazy.vaadin.components.ForgotPasswordComponent;
+import com.tooooolazy.vaadin.components.listeners.InputComponentButtonListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Notification;
@@ -28,14 +29,20 @@ public class MainView extends BaseView {
 	@Override
 	protected void addContent() {
 		// TODO Auto-generated method stub
-		ForgotPasswordComponent fpc = new ForgotPasswordComponent( new Button.ClickListener() {
-
-			@Override
-			public void buttonClick(ClickEvent event) {
-				// TODO Auto-generated method stub
-				Notification.show("button clicked");
-			}
+		ForgotPasswordComponent fpc = new ForgotPasswordComponent( new InputComponentButtonListener() {
 			
+			@Override
+			protected Object submit() throws Exception {
+				return null;
+			}
+			@Override
+			protected void onError(Exception e) {
+				super.onError( e );
+			}
+			@Override
+			protected void onSuccess(Object res) {
+				
+			}
 		}, this);
 		vl.addComponent( fpc );
 	}
