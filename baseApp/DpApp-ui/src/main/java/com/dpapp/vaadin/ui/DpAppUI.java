3177d3df002b8294c31abc8c40dd3377d789eec3
gpatoulas <gpatoulas.icap.gr>
2019-02-26 16:14:34 +0200

    new App for DeepAir

---
+++ baseApp/DpApp-ui/src/main/java/com/dpapp/vaadin/ui/DpAppUI.java
@@ -0,0 +1,90 @@
+package com.dpapp.vaadin.ui;
+
+import java.util.Properties;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.tooooolazy.util.Messages;
+import com.tooooolazy.vaadin.layout.TopAndLeftMenuLayout;
+import com.tooooolazy.vaadin.resources.Resources;
+import com.tooooolazy.vaadin.ui.BaseUI;
+import com.tooooolazy.vaadin.views.MainView;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Resource;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+
+@Theme("baseTheme")
+public class DpAppUI extends BaseUI<TopAndLeftMenuLayout> {
+
+	static {
+		Messages.setSupportedLocales( new String[] {"en", "el", "bg"});
+		try {
+			Messages.addBundle(DpAppUI.class.getPackage().getName() + ".app");
+		} catch(Exception e) {
+			// no default resource bundles... but never mind
+		}
+	}
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		super.init( vaadinRequest );
+	}
+
+	@Override
+	protected TopAndLeftMenuLayout createRootLayout() {
+		return new TopAndLeftMenuLayout();
+	}
+
+	@Override
+	protected Class getMainViewClass() {
+		return MainView.class;
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "DpAppUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = DpAppUI.class, productionMode = false)
+	public static class BaseUIServlet extends VaadinServlet {
+	}
+
+	@Override
+	protected Resource getLogoResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Resource getSecLogoResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Properties getEmailConfigProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supportsLocaleSwitching() {
+		return true;
+	}
+
+	@Override
+	public Resource getLocalSwitchResource() {
+		return Resources.getPng("img/actions/", "toggleLang3");
+	}
+
+	@Override
+	public Resource getLoginResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Resource getLogoutResource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
