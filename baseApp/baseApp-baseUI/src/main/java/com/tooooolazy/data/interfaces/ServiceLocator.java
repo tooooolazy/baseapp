a32ecaef31036d4a286d4021757026b61678fdaa
gpatoulas <gpatoulas.icap.gr>
2019-03-04 15:37:27 +0200

    ServiceLocator added

---
+++ baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/data/interfaces/ServiceLocator.java
@@ -0,0 +1,54 @@
+package com.tooooolazy.data.interfaces;
+
+
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceLocator {
+	private Map cache;
+	private static ServiceLocator ourInstance = new ServiceLocator();
+
+	public static ServiceLocator get() {
+		return ourInstance;
+	}
+
+	private ServiceLocator() {
+		this.cache = Collections.synchronizedMap(new HashMap());
+	}
+
+	public Object lookupSrv(Class srvClass) {
+		if (this.cache.containsKey(srvClass) && this.cache.get(srvClass) != null) {
+			return this.cache.get(srvClass);
+		} else {
+			try {
+				Object srvRef = createService( srvClass );
+				this.cache.put(srvClass, srvRef);
+				return srvRef;
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	private Object createService(Class srvClass) {
+//		if (srvClass.equals(DataHandlerService.class))
+//			return new DataHandler();
+//
+//		if (srvClass.equals(UserControllerService.class))
+//			return new UserController();
+//
+//		if (srvClass.equals(SecurityControllerService.class))
+//			return new SecurityController();
+//
+//		if (srvClass.equals(Icap_ControllerService.class))
+//			return new Icap_Controller();
+
+		return null;
+	}
+
+	public SecurityControllerService getSecurityController() {
+		return (SecurityControllerService) get().lookupSrv(SecurityControllerService.class);
+	}
+}
