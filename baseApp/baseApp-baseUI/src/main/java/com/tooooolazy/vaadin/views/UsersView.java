b180809d59acee75b53bf329bcc53ff0af3c4320
gpatoulas <gpatoulas.icap.gr>
2019-03-22 14:02:14 +0200

    Base UsersView (empty for now)

---
+++ baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/vaadin/views/UsersView.java
@@ -0,0 +1,17 @@
+package com.tooooolazy.vaadin.views;
+
+public class UsersView extends BaseView {
+
+	@Override
+	protected void addJavascriptFunctions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Class getCriteriaClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
