68fb915e17e7f79ced962de07e6ea9528f46cf74
gpatoulas <gpatoulas.icap.gr>
2019-03-22 14:01:44 +0200

    First User related Ws methods + general OnlineKeys

---
+++ baseApp/utils/src/main/java/com/tooooolazy/data/interfaces/OnlineKeys.java
@@ -0,0 +1,27 @@
+package com.tooooolazy.data.interfaces;
+
+import java.util.Arrays;
+
+public interface OnlineKeys {
+	public static String RESULT = "RESULT";
+
+	public static String ID = "ID";
+	public static String VALUE = "VALUE";
+	public static String DATA = "DATA";
+	public static String DATA_HEADERS = "DATA_HEADERS";
+
+	
+	// App params keys
+	public static String CATEGORY = "category";
+	public static String DESCRIPTION = "description";
+	public static String FAIL_MESSAGE = "failMessage";
+	public static String PARAMETER_VALUE = "parameterValue";
+
+
+	// Download PPM keys
+	public static String FILE_SIZE = "fileSize";
+	public static String FILE_PATH = "filePath";
+	public static String DATE_CREATED = "dateCreated";
+
+
+}
--- baseApp/utils/src/main/java/com/tooooolazy/data/interfaces/WsMethods.java
+++ baseApp/utils/src/main/java/com/tooooolazy/data/interfaces/WsMethods.java
@@ -9,4 +9,10 @@ public interface WsMethods {
 
 	public static String ENVIRONMENT = "getEnvironment";
 
+	public static String CLASS_TYPES = "getClassTypes";
+
+	public static String MSECLEVELDEFS = "getMSecLevelDefs"; // security data
+
+	public static String USERS = "getUsers";
+	public static String USERS_ROLES = "getUsersRoles";
 }
