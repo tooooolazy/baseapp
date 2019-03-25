ecd6662b7d18288ffc0a0489a3b180eb4d6d45fc
tooooolazy <tooooolazy@192.168.17.1>
2019-03-25 10:41:40 +0200

    MarketsGrid adjustments + a BaseGrid class added (depends on css)

--- baseApp/DpApp-ui/src/main/java/com/dpapp/vaadin/components/MarketsGrid.java
+++ baseApp/DpApp-ui/src/main/java/com/dpapp/vaadin/components/MarketsGrid.java
@@ -3,46 +3,71 @@ package com.dpapp.vaadin.components;
 import java.text.NumberFormat;
 
 import com.dpapp.data.beans.MarketBean;
-import com.dpapp.data.beans.MarketSectionBean;
 import com.tooooolazy.util.Messages;
-import com.vaadin.ui.Grid;
+import com.tooooolazy.vaadin.components.BaseGrid;
+import com.tooooolazy.vaadin.ui.BaseUI;
 import com.vaadin.ui.components.grid.HeaderCell;
 import com.vaadin.ui.components.grid.HeaderRow;
 import com.vaadin.ui.renderers.NumberRenderer;
 
-public class MarketsGrid extends Grid<MarketBean> {
+public class MarketsGrid extends BaseGrid<MarketBean> {
 
 	public MarketsGrid() {
 		super();
+		setId("MarketsGrid");
 
 		init();
 	}
 
 	protected void init() {
 		setBeanType(MarketBean.class);
+	}
+
+	public void attach() {
+		super.attach();
 
 		addColumns();
+		adjustMainHeader();
 		setGroupHeaders();
+	}
+
+	private void adjustMainHeader() {
+		HeaderRow mainHeader = getDefaultHeaderRow();
+		makeTwoLineHeader(mainHeader, "conversion.thisWeek", "thisWeek");
+		makeTwoLineHeader(mainHeader, "conversion.thisMonth", "thisMonth");
+		makeTwoLineHeader(mainHeader, "conversion.nextMonth", "nextMonth");
+
+		makeTwoLineHeader(mainHeader, "scale.thisWeek", "thisWeek");
+		makeTwoLineHeader(mainHeader, "scale.thisMonth", "thisMonth");
+		makeTwoLineHeader(mainHeader, "scale.nextMonth", "nextMonth");
+
+		makeTwoLineHeader(mainHeader, "revenue.thisWeek", "thisWeek");
+		makeTwoLineHeader(mainHeader, "revenue.thisMonth", "thisMonth");
+		makeTwoLineHeader(mainHeader, "revenue.nextMonth", "nextMonth");
+
+		makeTwoLineHeader(mainHeader, "revenueToPassenger.thisWeek", "thisWeek");
+		makeTwoLineHeader(mainHeader, "revenueToPassenger.thisMonth", "thisMonth");
+		makeTwoLineHeader(mainHeader, "revenueToPassenger.nextMonth", "nextMonth");
 	}
 
 	private void addColumns() {
 		addColumn( "market" ).setCaption( Messages.getString( getClass(), "market" ) );
 
-		addColumn( "conversion.thisWeek", new NumberRenderer( NumberFormat.getPercentInstance() ) ).setCaption( Messages.getString( getClass(), "thisWeek" ) );
-		addColumn( "conversion.thisMonth", new NumberRenderer( NumberFormat.getPercentInstance() )  ).setCaption( Messages.getString( getClass(), "thisMonth" ) );
-		addColumn( "conversion.nextMonth", new NumberRenderer( NumberFormat.getPercentInstance() )  ).setCaption( Messages.getString( getClass(), "nextMonth" ) );
+		addColumn( "conversion.thisWeek", new NumberRenderer( NumberFormat.getPercentInstance( BaseUI.get().getLocale() ) ) ).setStyleGenerator(market -> "red");
+		addColumn( "conversion.thisMonth", new NumberRenderer( NumberFormat.getPercentInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisMonth" ) ).setStyleGenerator(market -> "red");
+		addColumn( "conversion.nextMonth", new NumberRenderer( NumberFormat.getPercentInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "nextMonth" ) ).setStyleGenerator(market -> "green");
 
-		addColumn( "scale.thisWeek" ).setCaption( Messages.getString( getClass(), "thisWeek" ) );
-		addColumn( "scale.thisMonth" ).setCaption( Messages.getString( getClass(), "thisMonth" ) );
-		addColumn( "scale.nextMonth" ).setCaption( Messages.getString( getClass(), "nextMonth" ) );
+		addColumn( "scale.thisWeek", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisWeek" ) ).setStyleGenerator(market -> "red");
+		addColumn( "scale.thisMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisMonth" ) ).setStyleGenerator(market -> "red");
+		addColumn( "scale.nextMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "nextMonth" ) ).setStyleGenerator(market -> "green");
 
-		addColumn( "revenue.thisWeek" ).setCaption( Messages.getString( getClass(), "thisWeek" ) );
-		addColumn( "revenue.thisMonth" ).setCaption( Messages.getString( getClass(), "thisMonth" ) );
-		addColumn( "revenue.nextMonth" ).setCaption( Messages.getString( getClass(), "nextMonth" ) );
+		addColumn( "revenue.thisWeek", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisWeek" ) ).setStyleGenerator(market -> "red");
+		addColumn( "revenue.thisMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisMonth" ) ).setStyleGenerator(market -> "red");
+		addColumn( "revenue.nextMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "nextMonth" ) ).setStyleGenerator(market -> "green");
 
-		addColumn( "revenueToPassenger.thisWeek" ).setCaption( Messages.getString( getClass(), "thisWeek" ) );
-		addColumn( "revenueToPassenger.thisMonth" ).setCaption( Messages.getString( getClass(), "thisMonth" ) );
-		addColumn( "revenueToPassenger.nextMonth" ).setCaption( Messages.getString( getClass(), "nextMonth" ) );
+		addColumn( "revenueToPassenger.thisWeek", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisWeek" ) ).setStyleGenerator(market -> "red");
+		addColumn( "revenueToPassenger.thisMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "thisMonth" ) ).setStyleGenerator(market -> "red");
+		addColumn( "revenueToPassenger.nextMonth", new NumberRenderer( NumberFormat.getIntegerInstance( BaseUI.get().getLocale() ) ) ).setCaption( Messages.getString( getClass(), "nextMonth" ) ).setStyleGenerator(market -> "green");
 	}
 
 	private void setGroupHeaders() {
@@ -52,27 +77,27 @@ public class MarketsGrid extends Grid<MarketBean> {
 			    groupingHeader.getCell("conversion.thisMonth"),
 			    groupingHeader.getCell("conversion.nextMonth"));
 		conversionCell.setText( Messages.getString( getClass(), "conversion" ) );
+		conversionCell.setStyleName("centered");
 
 		HeaderCell scaleCell = groupingHeader.join(
 			    groupingHeader.getCell("scale.thisWeek"),
 			    groupingHeader.getCell("scale.thisMonth"),
 			    groupingHeader.getCell("scale.nextMonth"));
 		scaleCell.setText( Messages.getString( getClass(), "scale" ) );
+		scaleCell.setStyleName("centered");
 
 		HeaderCell revenueCell = groupingHeader.join(
 			    groupingHeader.getCell("revenue.thisWeek"),
 			    groupingHeader.getCell("revenue.thisMonth"),
 			    groupingHeader.getCell("revenue.nextMonth"));
 		revenueCell.setText( Messages.getString( getClass(), "revenue" ) );
+		revenueCell.setStyleName("centered");
 
 		HeaderCell revenueToPassengerCell = groupingHeader.join(
 			    groupingHeader.getCell("revenueToPassenger.thisWeek"),
 			    groupingHeader.getCell("revenueToPassenger.thisMonth"),
 			    groupingHeader.getCell("revenueToPassenger.nextMonth"));
 		revenueToPassengerCell.setText( Messages.getString( getClass(), "revenueToPassenger" ) );
-	}
-
-	public void attach() {
-		super.attach();
+		revenueToPassengerCell.setStyleName("centered");
 	}
 }
---
+++ baseApp/baseApp-baseUI/src/main/java/com/tooooolazy/vaadin/components/BaseGrid.java
@@ -0,0 +1,30 @@
+package com.tooooolazy.vaadin.components;
+
+import com.tooooolazy.util.Messages;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.components.grid.HeaderRow;
+
+/**
+ * Provides helper methods for all grids that extend this class
+ * @author tooooolazy
+ *
+ * @param <T>
+ */
+public class BaseGrid<T> extends Grid<T> {
+
+	/**
+	 * Makes a Grid header fit two lines of text by adjusting css and assuming that only 1 line break (<br>) exist. In order to work the following style must exist in theme:
+	 * <p><pre>#gridId .v-grid-cell.two-lines .v-grid-column-default-header-content {
+	 * 	line-height: 16px;
+	 * }</pre></p>
+	 * 
+	 * @param h - the HeaderRow to affect
+	 * @param colId - the column Id
+	 * @param captionKey - the key to look for in bundles (the value must have maximum 1 <br> defined in it otherwise UI will not look so good)
+	 */
+	protected void makeTwoLineHeader(HeaderRow h, String colId, String captionKey) {
+		h.getCell(colId).setHtml( Messages.getString( getClass(), captionKey ) );
+		h.getCell(colId).setStyleName("two-lines");
+	}
+
+}
